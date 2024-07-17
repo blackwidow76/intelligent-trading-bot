c74586cf33d64d0bac60017dbea9e78670ae38b8
blackwidow76 <98064208+blackwidow76@users.noreply.github.com>
2024-07-16 22:21:45 -0700

    Integrated PumpPortalClient and switched to MongoDB for database operations.

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,19 @@
+from re import M
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
-from .websocket_client import pump_fun_client, socketio  # Ensure this matches the correct file name
+from pumpportal.pumpportal_client import PumpPortalClient
 from .config import Config
+from pymongo import MongoClient
 import os
 import logging
 import asyncio
+from flask_socketio import SocketIO
 
 # Initialize Flask app
 app = Flask(__name__)
 app.config.from_object(Config)
-db = SQLAlchemy(app)  # Initialize SQLAlchemy with the app
+db = MongoClient(os.getenv('MONGO_URI')) # Initialize SQLAlchemy with the app
+socketio = SocketIO(app)
 
 # Setup logging
 logging.basicConfig(level=logging.INFO)
@@ -55,9 +59,9 @@ if __name__ == '__main__':
     test_pumpfun_integration()
     test_jito_integration()
     try:
-        asyncio.run(pump_fun_client())
+        client = PumpPortalClient()
+        asyncio.run(client.subscribe_new_token())
     except Exception as e:
         logger.error(f"Error running the WebSocket client: {str(e)}", exc_info=True)
     logger.info("Running Flask app with SocketIO")
-    socketio.init_app(app)
     socketio.run(app, debug=True, host='0.0.0.0', port=8080, use_reloader=False, log_output=True)
