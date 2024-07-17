5611a70a16ae48a7f4a941a22d6f53265798981d
blackwidow76 <98064208+blackwidow76@users.noreply.github.com>
2024-07-16 17:01:43 -0700

    Added price, volume, and contract_address fields to Token model.

--- backend/models.py
+++ backend/models.py
@@ -7,6 +7,9 @@ class Token(db.Model):
     name = db.Column(db.String(80), nullable=False)
     symbol = db.Column(db.String(10), nullable=False)
     launch_date = db.Column(db.DateTime, nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    volume = db.Column(db.Float, nullable=True)
+    contract_address = db.Column(db.String, unique=True, nullable=False)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
